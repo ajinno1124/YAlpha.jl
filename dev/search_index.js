03a3d975f3449af57e336f757dc251eb81270cab
Documenter.jl <documenter@juliadocs.github.io>
2023-09-13 03:19:58 +0000

    build based on 8e39de9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = YAlpha","category":"page"},{"location":"#YAlpha","page":"Home","title":"YAlpha","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for YAlpha.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [YAlpha]","category":"page"}]
+}
